2ef0affcd38e01049833b771d1511e33102585ff
Dipanshu <dipanshusaintra07@gmail.com>
2026-07-19 11:15:32 +0530

    resolve cors issue in usercontroller

--- habitTracker/src/main/java/com/dipanshu/habitTracker/controller/UserController.java
+++ habitTracker/src/main/java/com/dipanshu/habitTracker/controller/UserController.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/User")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 @Autowired
 private UserService userService;
